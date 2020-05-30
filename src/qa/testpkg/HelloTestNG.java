8eae798301a4940f2762f986cb2f65e2dc60f7dc
nirmal <jnirmal.1985@yahoo.com>
2020-05-30 10:47:07 +0530

    first commit

---
+++ src/qa/testpkg/HelloTestNG.java
@@ -0,0 +1,17 @@
+package qa.testpkg;
+
+import org.testng.annotations.Test;
+
+public class HelloTestNG {
+
+	@Test
+	public void helloTestNGHelloTest() {
+		System.out.println("Welcome to hello Test");
+	}
+	
+
+	@Test
+	public void helloTestNGHelloTest2() {
+		System.out.println(" Hello and By test Suite");
+	}
+}
---
+++ src/qa/testpkg/WelcomeTestNG.java
@@ -0,0 +1,18 @@
+package qa.testpkg;
+
+import org.testng.annotations.Test;
+
+public class WelcomeTestNG {
+
+	@Test
+	public void welcomeTestMessage() {
+		System.out.println("Welcome to TestNG framework.");
+	}
+	
+
+	@Test
+	public void helloBy() {
+		System.out.println(" Hello and By");
+	}
+
+}
